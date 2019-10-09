4a59b0b895291b89214a596bbc4960c23f6636df
Grzegorz Laszczak <39900077+glaszczak@users.noreply.github.com>
2019-10-09 11:47:42 +0200

    Delete database.js

--- config/database.js
+++
@@ -1,7 +0,0 @@
-/*
-buildingAgencyDB
-Nazwa bazy danych: grzegorzlasz_buildingAgencyDB
-Adres serwera baz danych: sql.grzegorzlasz.nazwa.pl
-Użytkownik bazy(login): grzegorzlasz_buildingAgencyDB
-Hasło użytkownika: Qwerty2019
-*/
